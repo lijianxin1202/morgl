a5bd95de5f1e8b28ba69d9e7c5e80b13d84d1e3e
jianxin <jianxin@xsky.com>
2019-04-12 16:43:33 +0800

    feat: Add test.

---
+++ src/algorithms/date/getWeekDay.test.ts
@@ -0,0 +1,8 @@
+import getWeekDay from "./getWeekDay";
+
+test("get week day", () => {
+  expect(getWeekDay(new Date(2019, 0, 1))).toBe(2);
+  expect(getWeekDay(new Date(2019, 1, 3))).toBe(0);
+  expect(getWeekDay(new Date(2019, 3, 5))).toBe(5);
+  expect(getWeekDay(new Date())).toBe(new Date().getDay());
+});
