3aafa2a27093459a9b10db7bdca0af097448acd1
jianxin <jianxin@xsky.com>
2019-04-25 12:00:22 +0800

    feat: Update test.

--- src/data-structures/queue/queue.test.ts
+++ src/data-structures/queue/queue.test.ts
@@ -4,7 +4,9 @@ test('queue', () => {
   const s = new Queue<number>();
   expect(s.isEmpty()).toBe(true);
   s.enQueue(3);
+  s.enQueue(4);
   expect(s.deQueue()).toBe(3);
+  s.deQueue();
   expect(s.isEmpty()).toBe(true);
   expect(s.deQueue).toThrow();
   expect(new Queue([2, 1, 3]).toString()).toBe('2,1,3');
